6ad2667fd18ed32f8540989a2d263601dc896364
Jordan-Skazinski <94482945+Jordan-Skazinski@users.noreply.github.com>
2022-01-13 09:24:25 -0700

    Update substitution.js

--- src/substitution.js
+++ src/substitution.js
@@ -24,7 +24,13 @@ const substitutionModule = (function () {
     //makes sure theirs no capital letters to mess me up
     let lowerCaseInput = input.toLowerCase()
     //an array of arrays with pairs of the actual alphabet with the new one.
-    let alphabetArray = [["a",alphabet[0]], ["b",alphabet[1]], ["c",alphabet[2]], ["d",alphabet[3]], ["e",alphabet[4]], ["f",alphabet[5]], ["g",alphabet[6]], ["h",alphabet[7]], ["i",alphabet[8]], ["j",alphabet[9]], ["k",alphabet[10]], ["l",alphabet[11]], ["m",alphabet[12]], ["n",alphabet[13]], ["o",alphabet[14]], ["p",alphabet[15]], ["q",alphabet[16]], ["r",alphabet[17]], ["s",alphabet[18]], ["t",alphabet[19]], ["u",alphabet[20]], ["v",alphabet[21]], ["w",alphabet[22]], ["x",alphabet[23]], ["y",alphabet[24]], ["z",alphabet[25]]]
+    let alphabetArray = [["a",alphabet[0]], ["b",alphabet[1]], ["c",alphabet[2]], ["d",alphabet[3]], 
+                         ["e",alphabet[4]], ["f",alphabet[5]], ["g",alphabet[6]], ["h",alphabet[7]], 
+                         ["i",alphabet[8]], ["j",alphabet[9]], ["k",alphabet[10]], ["l",alphabet[11]], 
+                         ["m",alphabet[12]], ["n",alphabet[13]], ["o",alphabet[14]], ["p",alphabet[15]], 
+                         ["q",alphabet[16]], ["r",alphabet[17]], ["s",alphabet[18]], ["t",alphabet[19]], 
+                         ["u",alphabet[20]], ["v",alphabet[21]], ["w",alphabet[22]], ["x",alphabet[23]], 
+                         ["y",alphabet[24]], ["z",alphabet[25]]]
     //if we want to decode
     if(!encode){
       //loops through the input array made earlier
